bc4eec47fe465827ec4414be0c7b9ba692cf27b0
Juan Tellez <jmtp12poke@gmail.com>
2025-05-31 19:58:13 -0600

    feat(*): implement aws sdk S3

--- main.js
+++ main.js
@@ -1,10 +1,19 @@
 import { app, BrowserWindow, ipcMain } from 'electron';
 import * as dgram from 'dgram';
 import fs from 'fs';
-import { start } from 'repl';
+import AWS from 'aws-sdk';
+import 'dotenv/config';
 
 const PORT = 20777;
 const outputFile = 'output.txt';
+
+// Configuración de AWS S3
+const s3 = new AWS.S3({
+  region: 'us-west-1', // Cambia esto a tu región
+  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
+});
+const BUCKET_NAME = 'demo-hipatia-bucket'; // Cambia esto a tu nombre de bucket
 
 let udpServer = null;
 
@@ -91,6 +100,34 @@ function startUpServer() {
   udpServer.bind(PORT);
 }
 
+function uploadFileToS3() {
+  return new Promise((resolve, reject) => {
+    fs.readFile(outputFile, (err, data) => {
+      if (err) {
+        console.error(`Error al leer el archivo: ${err}`);
+        reject(err);
+        return;
+      }
+      
+      const params = {
+        Bucket: BUCKET_NAME,
+        Key: `data-${Date.now()}.txt`, // Nombre único para cada archivo
+        Body: data
+      };
+      
+      s3.upload(params, (err, data) => {
+        if (err) {
+          console.error(`Error al subir a S3: ${err}`);
+          reject(err);
+          return;
+        }
+        console.log(`Archivo subido exitosamente a ${data.Location}`);
+        resolve(data);
+      });
+    });
+  });
+}
+
 function stopServer() {
   if (!udpServer) {
     console.log('The server is already stoped');
@@ -100,6 +137,15 @@ function stopServer() {
   udpServer.close(() => {
     console.log('UDP server closed');
     udpServer = null;
+    
+    // Subir el archivo a S3 cuando se detiene el servidor
+    uploadFileToS3()
+      .then(() => {
+        console.log('Proceso de subida a S3 completado');
+      })
+      .catch(err => {
+        console.error('Error en el proceso de subida a S3:', err);
+      });
   });
 }
 
